4908027289f890d39a802c22e852de711bf7849b
leodevbro <leodevbro@gmail.com>
2019-11-30 22:50:07 +0400

    First commit444

--- seeds.js
+++ seeds.js
@@ -14,7 +14,7 @@ let data = [
     },
     {
         name: "Desert Mesa",
-        image: "http://animalsbirds.com/wp-content/uploads/2016/07/chicken-nature-free-hd-wallpapers.jpg",
+        image: "https://i.pinimg.com/originals/2b/19/6c/2b196c6b5db67173a766a68a95c45505.jpg",
         description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
         author: {
             username: "rootUser"
@@ -22,7 +22,7 @@ let data = [
     },
     {
         name: "Canyon Floor",
-        image: "https://cdn3.vitalfarms.com/wp-content/uploads/2016/10/winterlight2.jpg",
+        image: "https://p1.pxfuel.com/preview/262/796/918/beautiful-dog-dog-shepherd-guard-handsome-man-nature.jpg",
         description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
         author: {
             username: "rootUser"
